876aa2dfbabf89b48c2ac0d32c5e6bfab4c8a781
Romain Wirth <wirth.romain@gmail.com>
2023-06-27 14:16:45 +0200

    ajout packages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [],
-  // TypeOrmModule.forRoot({
-  //   type:'sqlite',
-  //   database: 'parkingDatabase',
-  //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  //   synchronize: true,
-  // }),
+  //TypeOrmModule.forRoot({
+    //  type:'sqlite',
+    //  database: 'parkingDatabase',
+    //  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //  synchronize: true,
+  //  }),
   controllers: [AppController],
   providers: [AppService],
 })
